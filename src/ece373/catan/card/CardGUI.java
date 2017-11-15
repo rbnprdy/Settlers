768f5c102a3f8e2048549df200e6add85bc361b1
mills <mills@DESKTOP-IPG5JPQ>
2017-11-14 22:37:38 -0700

    more cardGUI elements

--- src/ece373/catan/card/CardGUI.java
+++ src/ece373/catan/card/CardGUI.java
@@ -3,10 +3,12 @@ package ece373.catan.card;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
+import java.awt.Image;
 import javax.swing.*;
 
 public class CardGUI extends JFrame {
 	private Card card;
+	private JLabel faceImageLabel;
 	
 	public CardGUI(Card new_card) {
 		card = new_card;
@@ -18,7 +20,17 @@ public class CardGUI extends JFrame {
 	}
 	
 	public void buildGUI() {
-		this.setSize(new Dimension(400,600));
-		//this.setUndecorated(true);		//REMOVING TOOLBAR
+		this.setSize(new Dimension(500,780));
+		this.setUndecorated(true);		//REMOVING TOOLBAR
+		faceImageLabel = new JLabel();
+		Image img = new ImageIcon(this.getClass().getResource("/brickcard.png")).getImage();
+		faceImageLabel.setSize(new Dimension(500,780));
+		if(card instanceof LargestArmyCard) {
+			
+		}
+		faceImageLabel.setIcon(new ImageIcon(img));
+		this.getContentPane().add(faceImageLabel);
+		
+		
 	}
 }
