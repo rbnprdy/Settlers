145380c6b4c3e463366ee8d914bc59a775df4923
Ruben Purdy <ruben.purdy@gmail.com>
2017-11-21 10:35:47 -0700

    Implemented button to add settlements and cities.

--- src/ece373/catan/board/NodeGUI.java
+++ src/ece373/catan/board/NodeGUI.java
@@ -3,6 +3,8 @@ package ece373.catan.board;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
+import javax.swing.JButton;
+
 public class NodeGUI {
 	
 	private Node n;
@@ -13,13 +15,24 @@ public class NodeGUI {
 	private Point center;
 	private int diameter = 25;
 	
+	private JButton button;
+	
 	public NodeGUI(Node n, Point center) {
 		this.n = n;
 		n.setGUI(this);
 		this.center = center;
+		this.button = null;
 		
 		circle = new Ellipse2D.Double(center.getX() - diameter + 7, center.getY() - diameter + 5, diameter, diameter);
 		square = new Rectangle((int) (center.getX() - (diameter/2+10)), (int) (center.getY() - (diameter/2+5)), diameter+5, diameter+5);
+	}
+	
+	public JButton getButton() {
+		return button;
+	}
+	
+	public void setButton(JButton button) {
+		this.button = button;
 	}
 	
 	public Point getCenter() {
